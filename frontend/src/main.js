92107961e1c4c188ded01b00540db04b78b88475
clydeH <clydehomu@gmail.com>
2022-04-25 00:59:49 +0200

    auth0 add globally to app

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,4 +1,5 @@
 import { createApp } from "vue";
+import { createAuth0 } from "@auth0/auth0-vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
@@ -7,4 +8,15 @@ import { loadFonts } from "./plugins/webfontloader";
 
 loadFonts();
 
-createApp(App).use(router).use(store).use(vuetify).mount("#app");
+createApp(App)
+  .use(router)
+  .use(store)
+  .use(vuetify)
+  .use(
+    createAuth0({
+      domain: "dev-rp-hfxja.us.auth0.com",
+      client_id: "Aclt6Ux35R2s1cVLuj5sdYUgP6miQACM",
+      redirect_uri: window.location.origin,
+    })
+  )
+  .mount("#app");

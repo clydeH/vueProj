4f3a6b3baf81a016e6fa60a612364155f8674f5e
clydeH <clydehomu@gmail.com>
2022-04-24 04:14:32 +0200

    first commit

---
+++ frontend/src/components/Nav-bar.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-card class="overflow-hidden">
+    <v-app-bar app elevation="6" color="#000000" dense dark>
+      <v-toolbar-title
+        ><span style="color: #fff">Vue showcase</span></v-toolbar-title
+      >
+      <v-spacer></v-spacer>
+      <v-btn color="#fff"> Login </v-btn>
+      <!-- -->
+    </v-app-bar>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "NavBar",
+
+  data: () => ({}),
+};
+</script>
---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="pageBackground">
+    <NavBar />
+    <ParticleEarth />
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+// Components
+import NavBar from "../components/Nav-bar.vue";
+import ParticleEarth from "../components/Particle.vue";
+
+export default defineComponent({
+  name: "HomeView",
+
+  components: {
+    NavBar,
+    ParticleEarth,
+  },
+});
+</script>
+
+<style scoped>
+.pageBackground {
+  background-color: #000000;
+  height: 100%;
+}
+</style>

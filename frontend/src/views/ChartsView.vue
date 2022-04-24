e8227285aa26ec9f1d2378d32fc381b8c7d5e683
clydeH <clydehomu@gmail.com>
2022-04-25 01:01:41 +0200

    componet chart

---
+++ frontend/src/views/ChartsView.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <NavBar />
+    <div class="flex">
+      <PieChart />
+      <ScatterChart />
+    </div>
+    <CandleChart />
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+// Components
+import NavBar from "../components/Nav-bar.vue";
+import PieChart from "../components/PieChart.vue";
+import ScatterChart from "../components/ScatterChart.vue";
+import CandleChart from "../components/CandleChart.vue";
+
+export default defineComponent({
+  name: "ChartsView",
+
+  components: {
+    NavBar,
+    PieChart,
+    ScatterChart,
+    CandleChart,
+  },
+});
+</script>
+
+<style scoped>
+.flex {
+  display: flex;
+}
+</style>
